93147010ecb1a3d05755bd07f1f9e7b61ff6d454
briaar <briarusss@gmail.com>
2023-09-18 17:14:41 -0400

    added error messages

--- workshop/src/components/SignUpForm.jsx
+++ workshop/src/components/SignUpForm.jsx
@@ -2,9 +2,37 @@
 import { useState } from "react";
 
 export default function SignUpForm({token,setToken}) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
   const [error, setError] = useState(null);
+
+  const handleUserName = (e) => {
+    if (e.target.value === null) {
+      setError("Please Enter username ");
+    }
+
+    if (e.target.value.length >= 20) {
+      setError("UserName is too long");
+    }
+    if (error && e.target.value.length < 20) {
+      setError("");
+    }
+  };
+
+  
+  const handlePassword = (e) => {
+    if (e.target.value === null) {
+      setError("Please Enter Password ");
+    }
+
+    if (e.target.value.length >= 20) {
+      setError("Password is too long");
+    }
+    if (error && e.target.value.length < 20) {
+      setError("");
+    }
+  };
+
 
   async function handleSubmit(e){
     e.preventDefault();
@@ -17,10 +45,13 @@ export default function SignUpForm({token,setToken}) {
         console.log(data);
         setToken(data.token);
         if(token!== null)console.log(token);
+        setUsername(null);
+        setPassword(null);
+        setSubMessage(data.message);
 
     }catch(e){
       console.error(e.message);
-      setError(e.message);
+      setError("Looks like something went, please try again");
     }
   }
 
@@ -29,7 +60,8 @@ export default function SignUpForm({token,setToken}) {
   return (
 
       <div>
-      <h2>Hello</h2>
+
+      <h2>Hello, Sign in to your account! </h2>
       {error && <p>{error}</p>}
       <form method="post" onSubmit={handleSubmit}>
         <label>
@@ -38,9 +70,9 @@ export default function SignUpForm({token,setToken}) {
         <label>
           Password: <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)}/><br></br>
         </label>
-        <button>Submit</button>
+        <button>Sign in </button>
       </form>
       </div>
-      
+
   );
 }
